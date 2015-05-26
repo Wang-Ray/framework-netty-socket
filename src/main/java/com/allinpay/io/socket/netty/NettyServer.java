df7389e93b59e671c27fd7df9dd26a07f92bc8d3
wanglei <wanglei@allinpay.com>
2015-05-26 14:47:20 +0800

    add netty support.

---
+++ src/main/java/com/allinpay/io/socket/netty/NettyServer.java
@@ -0,0 +1,54 @@
+package com.allinpay.io.socket.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+	public void bind(int port) throws Exception {
+		// 配置服务端的NIO线程组
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+					.option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new NettyServerHandler());
+						}
+					});
+			// 绑定端口，同步等待成功
+			ChannelFuture f = b.bind(port).sync();
+
+			// 等待服务端监听端口关闭
+			f.channel().closeFuture().sync();
+		} finally {
+			// 优雅退出，释放线程池资源
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		int port = 8080;
+		if (args != null && args.length > 0) {
+			try {
+				port = Integer.valueOf(args[0]);
+			} catch (NumberFormatException e) {
+				// 采用默认值
+			}
+		}
+		new NettyServer().bind(port);
+	}
+}
