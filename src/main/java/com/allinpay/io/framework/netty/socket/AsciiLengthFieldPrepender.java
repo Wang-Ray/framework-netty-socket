1c1443d445bc4abfacb64919984882735badd199
ray wang <ray.wang@onerway.com>
2024-03-27 11:28:59 +0800

    新增ascii长度域，从string到bytebuff的转换方法

--- src/main/java/com/allinpay/io/framework/netty/socket/AsciiLengthFieldPrepender.java
+++ src/main/java/com/allinpay/io/framework/netty/socket/AsciiLengthFieldPrepender.java
@@ -1,9 +1,11 @@
 package com.allinpay.io.framework.netty.socket;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
+import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -25,6 +27,7 @@ public class AsciiLengthFieldPrepender extends MessageToMessageEncoder<ByteBuf> 
     protected void encode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
         int length = msg.readableBytes();
         out.add(ctx.alloc().buffer(lengthFieldLength).writeBytes(String.format("%0"+lengthFieldLength+"d", length).getBytes(StandardCharsets.US_ASCII)));
+//        out.add(ByteBufUtil.encodeString(ctx.alloc(), CharBuffer.wrap(String.format("%0"+lengthFieldLength+"d", length)), StandardCharsets.US_ASCII));
         out.add(msg.retain());
     }
 }
